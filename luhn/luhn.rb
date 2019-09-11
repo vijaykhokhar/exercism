e0466c7e9724edd9f12603d45af638a39d583930
vijaykhokhar <vijaykhokhar312@gmail.com>
2019-09-11 19:09:16 +0530

    luhn alogrithm

--- luhn/luhn.rb
+++ luhn/luhn.rb
@@ -1,4 +1,26 @@
 class Luhn
-  def valid?(num)
+
+  class << self
+
+    def valid? number
+      number = number.gsub(/\s/, '') # remove all whitespace
+      number.match?(TWO_OR_MORE_DIGITS) && checksum_for(number).modulo(10).zero?
+    end
+
+    private
+
+    TWO_OR_MORE_DIGITS = /\A\d{2,}\z/
+
+    def double(digit)
+      (double = digit * 2) > 9 ? (double - 9) : double
+    end
+
+    def checksum_for number
+      number.chars.reverse.map(&:to_i).each_with_index.map { |digit, index|
+        index.even? ? digit : double(digit)
+      }.sum
+    end
+
   end
+
 end

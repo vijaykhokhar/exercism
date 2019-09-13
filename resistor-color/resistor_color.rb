b2864044bb4b4de13e7115e28e751ed3c487142a
vijaykhokhar <vijaykhokhar312@gmail.com>
2019-09-13 15:54:35 +0530

    resistor-color exercise

--- resistor-color/resistor_color.rb
+++ resistor-color/resistor_color.rb
@@ -1,6 +1,10 @@
 class ResistorColor
   COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+  
   def self.color_code(color)
-    COLORS[color].to_i
+    (0..COLORS.length).each do |place|
+      return place if COLORS[place] == color
+    end
+    'expected'    
   end
 end
